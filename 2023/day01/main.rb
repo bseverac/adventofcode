930d9a7114e90705d9d1a01ae9cadf32ca93db9a
bseverac <benjamin.severac@ilek.fr>
2023-12-02 19:42:30 +0100

    Add lint

--- 2023/day01/main.rb
+++ 2023/day01/main.rb
@@ -1,21 +1,23 @@
 #!/usr/bin/env ruby
-arr_1 = File.readlines("data_1.txt")
-arr_2 = File.readlines("data_2.txt")
+# frozen_string_literal: true
 
-NUMBERS = %w[one two three four five six seven eight nine]
+arr1 = File.readlines('data_1.txt')
+arr2 = File.readlines('data_2.txt')
+
+NUMBERS = %w[one two three four five six seven eight nine].freeze
 
 def string_to_number(str)
-    index = NUMBERS.find_index(str)
-    index ? index + 1 : str.to_i
+  index = NUMBERS.find_index(str)
+  index ? index + 1 : str.to_i
 end
 
-def first_and_last arr, regexp = /\d/
-    arr.map do
-        first = _1[/#{regexp}/]
-        last = _1[/.*(#{regexp}).*/, 1]
-        string_to_number(first) * 10 + string_to_number(last)
-    end
+def first_and_last(arr, regexp = /\d/)
+  arr.map do
+    first = _1[/#{regexp}/]
+    last = _1[/.*(#{regexp}).*/, 1]
+    string_to_number(first) * 10 + string_to_number(last)
+  end
 end
 
-puts first_and_last(arr_1).sum
-puts first_and_last(arr_2, /\d|#{NUMBERS.join('|')}/).sum
+puts first_and_last(arr1).sum
+puts first_and_last(arr2, /\d|#{NUMBERS.join('|')}/).sum
