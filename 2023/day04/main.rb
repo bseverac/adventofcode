37653e44938f461a9cfc2ca8078a93e25db6f2a2
bseverac <benjamin.severac@ilek.fr>
2023-12-04 18:01:59 +0100

    day 04

---
+++ 2023/day04/main.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+arr1 = File.readlines('data_1.txt')
+arr1 = arr1.map(&:strip)
+
+card_score = arr1.map { |e| e.match(/Card *\d*: (.*) \| (.*)/).captures }.map do |e|
+  e[0].split.map(&:to_i).intersection(e[1].split.map(&:to_i)).count
+end
+
+# star1
+card_score.filter(&:positive?).map { |e| 2.pow(e - 1) }.sum
+
+copies = {}
+card_score.map.with_index do |e, index|
+  copies[index] ||= 1
+  e.times do |i|
+    copies[index + i + 1] ||= 1
+    copies[index + i + 1] += copies[index]
+  end
+end
+
+# star2
+puts copies.values.sum
